e1ec1528a0e8974e6e5d7be2a2066e280f1b11aa
DanteFromMars <ivelina.karavalcheva@gmail.com>
2016-08-28 19:51:36 +0300

    Update homeControler.js

--- BlogWithCustomFramework/scripts/controllers/homeControler.js
+++ BlogWithCustomFramework/scripts/controllers/homeControler.js
@@ -3,11 +3,24 @@ class HomeController{
         this._homeview = homeView;
         this._requester = requester;
         this._appKey = appKey;
-        this._BaseServiceUrl = baseUrl;//TODO
+        this._BaseServiceUrl = baseUrl + "/appData/" + appKey + "/posts";
         
     }
 
     showGuestPage(){
-        this._homeview.showGuestPage();
+        let _that = this;
+        this.requester.get(this._BaseServiceUrl,
+        function(response){
+            showPopup('success','Successfully collected data.');
+            that._homeview.showGuestPage(response);
+        },
+        function(data){
+            showPopup('error','Error collecting the data.')
+        })
+
+    }
+
+    showUserPage(){
+        this._homeview.showUserPage();
     }
 }
